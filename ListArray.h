20ba4016f6ee1626b29bd2f9413309a403a948b9
Gonzalo Albelda Barona <galbbar@upv.edu.es>
2023-10-24 18:42:24 +0200

    Añadido

--- ListArray.h
+++ ListArray.h
@@ -2,8 +2,6 @@
 #include "List.h"
 #include <vector>
 
-usingnamespace std;
-
 template <typename T>
 class ListArray : public List<T> 
 {
@@ -41,15 +39,15 @@ class ListArray : public List<T>
 			delete[] arr;
 		}
 
-		T oeprator[](int pos)
+		T operator[](int pos)
 		{
-			if(pos < 0 || pos > max-1)
-			       	throw out_of_range("La posicion no es valida para el vector");
+			if(pos < 0 || pos > size()-1)
+			       	throw std::out_of_range("La posicion no es valida para el vector");
 			else
 				return arr[pos];	
 		}
 		
-		friend ostream& operator<<(ostream &out, const ListArray<T> &list)
+		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list)
 		{
 			out << "List -> [";
 			for(int i=0; i<list.n; i++)
@@ -58,5 +56,87 @@ class ListArray : public List<T>
 			return out;
 		}
 
+		virtual void insert(int pos, T e) override
+		{
+			if(pos < 0 || pos > size()-1)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			else 
+			{
+				if(pos == 0)
+					prepend(e);
+				else
+				{
+					if(pos == n)
+						append(e);
+					else
+					{
+						resize(size()+1);
+						for(int i=size()-1; i>=pos; i++)
+							arr[i+1] = arr[i]; 
+						arr[pos-1] = e;
+					}
+				}
+			}
+		}
+
+		virtual void append(T e) override
+		{
+			resize(size()+1);
+			arr[size()-1]=e;
+		}
+
+		virtual void prepend(T e) override
+		{
+			resize(size()+1);
+			for(int i=size()-1;i>0;i--)
+				arr[i+1] = arr[i];
+			arr[0] = e;
+		}
+
+		virtual T remove(int pos) override
+		{
+			if(pos < 0 || pos > size()-1)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			else
+			{
+				T aux = arr[pos-1];
+	
+				for(int i=pos-1; i<size(); i++)
+					arr[i] = arr[i+1];
+				resize(size()-1);
+	
+				return aux;
+			}
+		}
+
+		virtual T get(int pos) override
+		{
+			if(pos < 0 || pos > size()-1)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			else
+				return arr[pos];
+		}
+
+		virtual int search(T e) override
+		{
+			int j=-1;
+			for(int i=0; i<n; i++)
+				if(arr[i] == e)
+					j=i;
+			return j;
+		}
+
+		virtual bool empty() override
+		{
+			if(n == 0)
+				return true;
+			else
+				return false;	
+		}
+
+		virtual int size() override
+		{
+			return n;
+		}
 
 };
