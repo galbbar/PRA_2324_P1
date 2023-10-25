c4c571fb7301924c305e582402de1657f996afc8
Gonzalo Albelda Barona <galbbar@upv.edu.es>
2023-10-25 16:37:59 +0200

    Afegit

--- ListArray.h
+++ ListArray.h
@@ -1,6 +1,5 @@
 #include <ostream>
 #include "List.h"
-#include <vector>
 
 template <typename T>
 class ListArray : public List<T> 
@@ -13,24 +12,29 @@ class ListArray : public List<T>
 
 		void resize(int new_size)
 		{
-			T arr_aux[new_size];
-			for(int i = 0; i<n ; i++)
-				arr_aux[i] = arr[i];
+		  	T aux[new_size];
+		  
+		  	for(int i =0 ;i<new_size;i++)
+		  	{
+			  	aux[i] = arr[i];
+		  	}
 
-			delete[] arr;
+		  	delete arr;
 
-			arr = new int[new_size];
-			for(int j = 0; j<new_size; j++)
-				arr[j] = arr_aux[j];
-
-			max = new_size;
-		}
+		  	arr = new int [new_size];
+		  	for (int i=0;i<new_size;i++)
+		  	{
+			  	arr[i] = aux[i];
+			  	max = new_size ;
+		  	}
+	    
+  		}
 
 	public:
 		ListArray()
 		{
-			arr = new int[MINISIZE];
-			max = MINISIZE;
+			arr = new T[MINISIZE];
+			max = 2;
 			n = 0;
 		}
 		
@@ -39,6 +43,94 @@ class ListArray : public List<T>
 			delete[] arr;
 		}
 
+		virtual void insert(int pos, T e) override
+		{
+			if(pos < 0 || pos > n)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			if(size() == max)
+				resize(size()*2);
+				
+			if(pos == 0)
+				prepend(e);
+			else
+			{
+				if(pos == n)
+					append(e);
+				else
+				{
+					for(int i=n; i>pos; i--)
+						arr[i] = arr[i-1]; 
+					arr[pos] = e;
+					n++;
+				}
+			}
+			
+		}
+		
+		virtual void append(T e) override
+		{ 
+			
+			if(size() == max)
+             			resize(size()*2);
+			arr[n] = e;
+			n++;
+		}
+
+		virtual void prepend(T e) override
+		{ 
+		
+			if(size() == max)
+	     			resize(size()*2);
+      			else
+      			{
+	      			for(int i = n; i > 0; i --)
+		      			arr[i] = arr[i-1];	
+				arr[0] = e;
+	      			n++;
+      			} 
+		}
+
+		virtual T remove(int pos) override
+		{
+			if(pos < 0 || pos > size()-1)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			T aux = arr[pos];
+
+			for(int i=pos; i<n; i++)
+				arr[i] = arr[i+1];
+			n--;
+
+			return aux;
+		}
+
+		virtual T get(int pos) override
+		{
+			if(pos < 0 || pos > size()-1)
+				throw std::out_of_range("La posicion no es valida para el vector");
+			return arr[pos];
+		}
+
+		virtual int search(T e) override
+		{
+			for(int i=0; i<n; i++)
+				if(arr[i] == e)
+					return i;
+			return -1;
+		}
+
+		virtual bool empty() override
+		{
+			if(n == 0)
+				return true;
+			else
+				return false;	
+		}
+
+		virtual int size() override
+		{
+			return n;
+		}
+		
 		T operator[](int pos)
 		{
 			if(pos < 0 || pos > n)
@@ -50,91 +142,9 @@ class ListArray : public List<T>
 		{
 			out << "List -> [";
 			for(int i=0; i<list.n; i++)
-				out << list.arr[i] << ", ";
+				out << list.arr[i] <<" ";
 			out << "]";
 			return out;
 		}
-
-		virtual int size() override
-		{
-			return n;
-		}
 		
-		virtual void append(T e) override
-		{
-			resize(size()+1);
-			arr[size()-1]=e;
-		}
-
-		virtual void prepend(T e) override
-		{
-			resize(size()+1);
-			for(int i=size()-1;i>0;i--)
-				arr[i+1] = arr[i];
-			arr[0] = e;
-		}
-		
-		virtual void insert(int pos, T e) override
-		{
-			if(pos < 0 || pos > n)
-				throw std::out_of_range("La posicion no es valida para el vector");
-			if(size() == 0)
-				resize(size()*2);
-				
-			if(pos == 0)
-				prepend(e);
-			else
-			{
-				if(pos == n)
-					append(e);
-				else
-				{
-					resize(size()+1);
-					for(int i=size()-1; i>=pos; i--)
-						arr[i+1] = arr[i]; 
-					arr[pos-1] = e;
-				}
-			}
-			
-		}
-
-		
-
-		virtual T remove(int pos) override
-		{
-			if(pos < 0 || pos > n)
-				throw std::out_of_range("La posicion no es valida para el vector");
-			T aux = arr[pos-1];
-
-			for(int i=pos-1; i<size(); i++)
-				arr[i] = arr[i+1];
-			resize(size()-1);
-
-			return aux;
-		}
-
-		virtual T get(int pos) override
-		{
-			if(pos < 0 || pos > n)
-				throw std::out_of_range("La posicion no es valida para el vector");
-			return arr[pos];
-		}
-
-		virtual int search(T e) override
-		{
-			int j=-1;
-			for(int i=0; i<n; i++)
-				if(arr[i] == e)
-					j=i;
-			return j;
-		}
-
-		virtual bool empty() override
-		{
-			if(n == 0)
-				return true;
-			else
-				return false;	
-		}
-
 };
